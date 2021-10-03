34b7c27d45ce8dbef7bc55321edf645fb1905ae0
kevinbabou <kevincbabou@gmail.com>
2021-10-03 01:17:20 -0400

    fully functioning to-do list app using .plists for local data persistence

---
+++ Todoey/Controllers/NavigationViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NavigationBarViewController.swift
+//  NavigationBarViewController
+//
+//  Created by Kevin Babou on 10/2/21.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NavigationViewController: UINavigationController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Defining our screen title
+        
+    }
+}
---
+++ Todoey/Controllers/TodoListViewController.swift
@@ -0,0 +1,145 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Philipp Muellauer on 02/12/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    var itemArray = [Item]()
+    
+    let defaults = UserDefaults.standard
+    
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        setTitleAndAppearance()
+        loadItems()
+    }
+
+    //MARK: - Table View Data Source Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+        
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        //Ternary operator ==>
+        // value = condition ? valueIfTrue : valueIfFalse
+        cell.accessoryType = item.done ? .checkmark : .none
+        
+        return cell
+    }
+    
+    //MARK: - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //print("You've selected " + itemArray[indexPath.row])
+        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        saveItems()
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    //MARK: - Add New Items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            //what will happen when the user clicks the Add Item button on our UIAlert
+            
+            let newItem = Item()
+            newItem.title = textField.text!
+            
+            self.itemArray.append(newItem)
+            
+            self.saveItems()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+}
+
+//MARK: - Extending the TodoListViewController to include acustom
+//setTitleAndAppearance function since the attribute inspector method
+//of setting a custom navigation bar color seems to be buggy.
+extension TodoListViewController {
+    func setTitleAndAppearance() {
+        self.title = "Todoey"
+
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = .systemBlue
+        appearance.titleTextAttributes = [.font: UIFont.boldSystemFont(ofSize: 20.0),
+                                          .foregroundColor: UIColor.white]
+
+        // Customizing our navigation bar
+        navigationController?.navigationBar.tintColor = .white
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+    }
+}
+
+//MARK: - Extending the TodoViewListController to include a reusable
+//saveItems function which initializes an instance of
+//a property list (.plist) encoder object
+//The encoder object's main function, .encode(),
+//must be called within a "do { try } catch {}" block
+extension TodoListViewController {
+    func saveItems() {
+        let encoder = PropertyListEncoder()     //declare an encoder
+                                                //which is an instance of a PropertyListEncoder()
+                                                //object, which  will encode our itemArray into
+                                                //data that conforms to a .plist file key-value format.
+        
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        } catch {
+            print("Error enconding item array: \(error)")
+        }
+        tableView.reloadData()
+    }
+
+}
+
+extension TodoListViewController {
+    func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error decoding item: \(error)" )
+            }
+            
+        }
+    }
+}
